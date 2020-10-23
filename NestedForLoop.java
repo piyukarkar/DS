6d9fe4b0f0c5b15d67422ae113e6291c823a0056
Priyanka Karkar <karkarpriyanka@yahoo.com>
2020-10-23 12:13:39 +0530

    ds

---
+++ NestedForLoop.java
@@ -0,0 +1,17 @@
+public class NestedForLoop {
+    
+
+	public static void main(String[] args) {
+		
+		for(int j=1; j<=50; j++) {
+			
+		
+		
+		for(int i =0;i<=10; i++) {
+			System.out.print(i+" ");
+		}
+             System.out.println();
+	}
+
+}
+}
