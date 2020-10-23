32860424c9d60a2d1b81838a5a30caf96d493025
Priyanka Karkar <karkarpriyanka@yahoo.com>
2020-10-23 13:45:12 +0530

    ds

---
+++ Pattern7.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Pattern7 {
+    public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n= sc.nextInt();
+		
+		for(int i =1; i<=n;i++) {
+			for(int j= 1; j<=n-1;j++) {
+				System.out.print("  ");
+			
+		}
+		}
+      
+           System.out.println("*");
+        
+           for(int i =1; i<=n;i++) {
+   			for(int j= 1; j<=n-1;j++) {
+   				System.out.print("  ");
+   			}
+   			
+           }
+        for(int i =2;i<=n-1;i++) {
+       	 System.out.print("* ");
+       	 
+       	 for(int j = 1; j<=i-2;j++){
+       			 
+       			 System.out.print("  ");
+        }
+        System.out.print("* ");
+       
+       
+        System.out.println();       
+       
+        }
+       for(int i=1;i<=n;i++) {
+       	System.out.print("* ");
+       }
+
+	}
+}
